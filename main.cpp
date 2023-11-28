d638dc8ef4b8b34b8ebe1c41ef8d587b1af2b522
Salam <salamabd@umich.edu>
2023-11-28 18:46:47 -0500

    error handling for main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,31 @@
 #include <iostream>
+#include "BinarySearchTree.hpp"
+#include "Map.hpp"
+#include "csvstream.hpp"
 
-int main() {
+using namespace std;
 
+int main(int argc, char* argv[]) {
+    if ((argc != 4 && argc != 3) || (argc == 4 && argv[3] != "--debug")) { 
+        cout << "Usage: main.exe TRAIN_FILE TEST_FILE [--debug]" << endl;
+        return 1; 
+    }
+
+    try {
+        csvstream trainStream(argv[1]);
+    }
+
+    catch (csvstream_exception failToOpen) {
+        cout << "Error opening file: " << argv[1] << endl;
+        return 1;
+    }
+
+    try {
+        csvstream testStream(argv[2]);
+    }
+
+    catch (csvstream_exception failToOpen) {
+        cout << "Error opening file: " << argv[2] << endl;
+        return 1;
+    }
 }
